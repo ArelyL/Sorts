2f822d926267def1b515a71ecc8e3b1107cc57cd
ArelyL <arely_4195@hotmail.com>
2017-09-28 14:57:17 -0500

    Add files via upload
    The file InsertSort.py was add

--- InsertSort.py
+++ InsertSort.py
@@ -2,54 +2,37 @@
 
 import random
 
-n=10
+n=100
 
-def InsertSort(A, B):
- 
-    if B<=A[0]: A=[B]+A
-    elif A[n-1]<=B: A=A+[B]
-    else:
-        for i in range(0, n-1):
-            if A[i]<=B and B<=A[i+1]:
-                A.insert(i+1,B)
-                break
-    return A
+def InsertSort(A):
 
-def BubbleSort(A):
-    m=0
-    for i in range(0,n-1):
-        for j in range(1,n-i):
-            if A[j]<A[j-1]:
-                A[j],A[j-1]=A[j-1],A[j]
-                m=1
-        if m==0: break
-        m=0
+    M=[A[0]]
+    k=2
 
-    return A  
-    
-    
+    for i in range(1,n):
+        if A[i]<M[0]:
+            M=[A[i]]+M
+        elif A[i]>=M[len(M)-1]:
+            M=M+[A[i]]
+        else:
+            for j in range(1,len(M)):
+                if A[i]>=M[j-1] and A[i]<M[j]:
+                    M.insert(j,A[i])
+                    break
+                k+=1
+            
+    #print(M)
+
 
 if __name__=='__main__':
     A=[0 for i in range(n)]
 
     for i in range(n):
-        A[i]=random.randint(0,n-1)
+        A[i]=random.randint(0,n)
 
-    print("Los numeros a ordenar son\n")
-    print(A)
+    #print("Los numeros a ordenar son\n", A)
+
+    #print("\nLos numeros ordenados son\n")
+    InsertSort(A)
+
         
-    M=BubbleSort(A)
-    print("\n\nLos numeros ordenados son\n")
-    print(M)
-
-    R=InsertSort(M,6)
-    print("\n\nSe inserto el numero 5 en la cadena\n")
-    print(R)
-    
-    R=InsertSort(R,256)
-    print("\n\nSe inserto el numero 256 en la cadena\n")
-    print(R)
-
-    R=InsertSort(R,-3)
-    print("\n\nSe inserto el numero -3 en la cadena\n")
-    print(R)

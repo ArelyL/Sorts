7ca8935cd87955e655ade570206ab8ed6ee0edf0
ArelyL <arely_4195@hotmail.com>
2017-10-25 13:44:21 -0500

    The file "QuickSort.cpp" was added

---
+++ QuickSort.cpp
@@ -0,0 +1,84 @@
+/* C implementation QuickSort */
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+#define N 10000
+
+// A utility function to swap two elements
+void Swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+    array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+int Partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            Swap(&arr[i], &arr[j]);
+        }
+    }
+    Swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,
+  low  --> Starting index,
+  high  --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi =Partition(arr, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+/* Function to print an array */
+void printArray(int arr[], int Size)
+{
+    int i;
+    for (i=0; i <Size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver program to test above functions
+int main()
+{
+    //int arr[] = {10, 7, 8, 9, 1, 5};
+    int n=N;
+    int arr[N];
+    for(int i=0; i<N;i++){
+		arr[i]= rand()%N;
+		//printf("%i ", A[i]);
+	}
+
+
+    quickSort(arr, 0, n-1);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}

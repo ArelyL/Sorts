d5993aa4f5159f5ca0cc89c98e217d185a56a7d3
ArelyL <arely_4195@hotmail.com>
2017-10-25 12:00:34 -0500

    The file "QuickSort.py" was added

---
+++ QuickSort.py
@@ -0,0 +1,70 @@
+# Python program for implementation of Quicksort Sort
+ 
+# This function takes last element as pivot, places
+# the pivot element at its correct position in sorted
+# array, and places all smaller (smaller than pivot)
+# to left of pivot and all greater elements to right
+# of pivot
+
+
+
+#!/env/bin/python
+
+import random
+import math
+
+n=10000
+
+
+def partition(arr,low,high):
+    i = ( low-1 )         # index of smaller element
+    pivot = arr[high]     # pivot
+ 
+    for j in range(low , high):
+ 
+        # If current element is smaller than or
+        # equal to pivot
+        if   arr[j] <= pivot:
+         
+            # increment index of smaller element
+            i = i+1
+            arr[i],arr[j] = arr[j],arr[i]
+ 
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+    return ( i+1 )
+ 
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low  --> Starting index,
+# high  --> Ending index
+ 
+# Function to do Quick sort
+def quickSort(arr,low,high):
+    if low < high:
+ 
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr,low,high)
+ 
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+ 
+if __name__=='__main__':
+    N=[0 for i in range(n)]
+
+    for i in range(n):
+        N[i]=random.randint(0,n)
+        
+print "Array is:", N
+
+quickSort(N,0,n-1)
+
+print "Sorted array is:", N
+
+
+##for i in range(n):
+##    print ("%d" %N[i]),
+ 
+# This code is contributed by Mohit Kumra
